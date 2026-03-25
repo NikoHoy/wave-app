2882f4529af5ac1522c768681836ed4924706067
Daniel Pihl <daniel.pihl@student.oulu.fi>
2026-03-25 17:17:14 +0200

    speaker types

--- demo/src/main/java/wave/app/WaveSource.java
+++ demo/src/main/java/wave/app/WaveSource.java
@@ -5,11 +5,13 @@ public class WaveSource {
         int emitRate = 10; // Emit wave every 5 frames
         int frameCounter = 0;
         int emitAngle=0;
+        double amplitude=1;
         
-        WaveSource(double x, double y, int emitAngle) {
+        WaveSource(double x, double y, int emitAngle, double amplitude) {
             this.x = x;
             this.y = y;
             this.emitAngle=emitAngle;
+            this.amplitude=amplitude;
 
         }
     }
